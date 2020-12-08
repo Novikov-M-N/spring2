e4a449fc9d2edbdc1bb053f9c53cd9c53a62cce3
Novikov-M-N <mnovikov37@gmail.com>
2020-12-08 23:56:59 +0700

    lesson4Homework

--- src/main/java/com/github/novikovmn/spring2/config/WebConfig.java
+++ src/main/java/com/github/novikovmn/spring2/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.github.novikovmn.spring2.config;
 
+import com.github.novikovmn.spring2.domain.Dto;
+import com.github.novikovmn.spring2.domain.PayloadDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -19,9 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private SimpMessagingTemplate template;
 
-    @Scheduled(fixedRate = 5000L)
+    @Scheduled(fixedRate = 3000L)
     private void sendItem() {
         template.convertAndSend("/topic", "test message");
+        template.convertAndSend("/topic", new PayloadDTO(1, "hello"));
     }
 
 }
---
+++ src/main/java/com/github/novikovmn/spring2/domain/Dto.java
@@ -0,0 +1,14 @@
+package com.github.novikovmn.spring2.domain;
+
+public class Dto {
+//    private Integer i;
+    private String s;
+
+    public Dto(String s) {
+//        this.i = i;
+        this.s = s;
+    }
+
+    public void setS(String s) { this.s = s; }
+    public String getS() { return s; }
+}
